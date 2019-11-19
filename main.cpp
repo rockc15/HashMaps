506116bf8ed2a2295e7a7dc545dbcf9a78b10aa5
rockc15 <rockc@udel.edu>
2019-11-19 10:16:57 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 19, 2019
+ *      Author: casey
+ */
+
+#include "hashMap.hpp"
+#include "makeSeuss.hpp"
+
+
+int main(){
+cout << "casey" << endl;
+
+return 0;
+}
